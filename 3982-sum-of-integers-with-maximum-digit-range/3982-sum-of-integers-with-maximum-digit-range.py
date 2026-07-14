11b5ae2277878a0cbcb4777eee0efdfe921114db
Anuj Singh <anujmaurya7967@gmail.com>
2026-07-14 19:05:06 +0530

    Time: 8 ms (74.47%), Space: 19.3 MB (36.1%) - LeetHub

--- 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.py
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.py
@@ -1,15 +1,31 @@
+# class Solution:
+#     def maxDigitRange(self, nums: list[int]) -> int:
+        
+#         n = len(nums)
+#         digits = defaultdict(int)
+#         for i in range(n):
+#             s = str(nums[i])
+#             maxi , mini = int(max(s)) , int(min(s))
+#             digits[maxi-mini] += nums[i]
+        
+#         return digits[max(digits)]
+
+
+
 class Solution:
     def maxDigitRange(self, nums: list[int]) -> int:
-        
-        n = len(nums)
-        digits = defaultdict(int)
-        for i in range(n):
-            s = str(nums[i])
-            maxi , mini = int(max(s)) , int(min(s))
-            digits[maxi-mini] += nums[i]
-        
-        return digits[max(digits)]
 
+        best = -1
+        ans = 0
 
+        for num in nums:
+            s = str(num)
+            diff = int(max(s)) - int(min(s))
 
+            if diff > best:
+                best = diff
+                ans = num
+            elif diff == best:
+                ans += num
 
+        return ans
