2ff2bba85efcb591c3cdf0d04d4addb6147c3723
Anuj Singh <anujmaurya7967@gmail.com>
2026-07-29 20:05:32 +0530

    Time: 969 ms (8.7%), Space: 48.2 MB (36.96%) - LeetHub

--- 3244-shortest-distance-after-road-addition-queries-ii/3244-shortest-distance-after-road-addition-queries-ii.py
+++ 3244-shortest-distance-after-road-addition-queries-ii/3244-shortest-distance-after-road-addition-queries-ii.py
@@ -1,37 +1,66 @@
+# class Solution:
+#     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        
+#         nxt = list(range(1,n+1))
+#         nxt[n-1] = -1
+#         curr_dist = n-1
+#         ans = []
+        
+#         for u , v in queries :
+
+#             if nxt[u] == -1 or nxt[u] >= v :
+#                 ans.append(curr_dist)
+            
+#             else :
+
+#                 curr = nxt[u]
+#                 while curr != v :
+#                     nxt[curr] , curr = -1 , nxt[curr]
+#                     curr_dist -= 1
+                
+#                 nxt[u] = v
+#                 ans.append(curr_dist)
+            
+#                 # curr = u
+#                 # while curr < v :
+#                 #     temp = nxt[curr]
+#                 #     curr_dist -= 1
+#                 #     curr = temp
+                
+#                 # curr_dist += 1
+#                 # nxt[u] = v
+
+#             # ans.append(curr_dist)
+        
+#         return ans
+
+from sortedcontainers import SortedList
+from typing import List
+
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        
-        nxt = list(range(1,n+1))
-        nxt[n-1] = -1
-        curr_dist = n-1
+        # Active nodes in the path from 0 to n-1
+        active_nodes = SortedList(range(n))
         ans = []
-        
-        for u , v in queries :
 
-            if nxt[u] == -1 or nxt[u] >= v :
-                ans.append(curr_dist)
-            
-            else :
+        for u, v in queries:
+            # Check if u and v are both still active in our path
+            idx_u = active_nodes.bisect_left(u)
+            idx_v = active_nodes.bisect_left(v)
 
-                curr = nxt[u]
-                while curr != v :
-                    nxt[curr] , curr = -1 , nxt[curr]
-                    curr_dist -= 1
-                
-                nxt[u] = v
-                ans.append(curr_dist)
-            
-                # curr = u
-                # while curr < v :
-                #     temp = nxt[curr]
-                #     curr_dist -= 1
-                #     curr = temp
-                
-                # curr_dist += 1
-                # nxt[u] = v
+            # If u is in the set and v is strictly to the right of u
+            if idx_u < len(active_nodes) and active_nodes[idx_u] == u:
+                # Remove all nodes strictly between u and v
+                # Note: bisect_right(u) finds the first element > u
+                start_remove = active_nodes.bisect_right(u)
+                end_remove = active_nodes.bisect_left(v)
 
-            # ans.append(curr_dist)
-        
+                # Delete slices of nodes that are bypassed
+                del active_nodes[start_remove:end_remove]
+
+            # Current distance is number of active nodes - 1
+            ans.append(len(active_nodes) - 1)
+
         return ans
 
 
