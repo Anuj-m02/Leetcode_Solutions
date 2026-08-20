8d66dda15d7f10b92bc2cb4f17ae23553e4c6376
Anuj Singh <anujmaurya7967@gmail.com>
2026-08-20 14:30:09 +0530

    Time: 0 ms (100%), Space: 27.9 MB (38.32%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,62 @@
+# from collections import defaultdict , deque , Counter
+# import heapq
+# from functools import lru_cache
+
+
+# class Solution:
+#     def canReach(self, arr: List[int], start: int) -> bool:
+
+#         n = len(arr)
+#         visited = set()        
+
+#         @lru_cache(maxsize=None)
+#         def dp(indx) :
+
+#             if arr[indx] == 0 :
+#                 return True
+            
+#             if indx in visited :
+#                 return False
+            
+#             ans = False
+#             op1 , op2 = min(indx + arr[indx] , n-1) , max(indx - arr[indx] , 0)
+#             visited.add(op1)
+#             visited.add(op2)
+
+#             if dp(op1) or dp(op2) :
+#                 return True
+            
+#             return False
+        
+#         return dp(start)
+
+from collections import defaultdict, deque, Counter
+import heapq
+from functools import lru_cache
+from typing import List
+
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+
+        n = len(arr)        
+        visited = set()
+
+        def dp(indx):
+            # Base cases: out of bounds or loop detected
+            if indx < 0 or indx >= n or indx in visited:
+                return False
+
+            if arr[indx] == 0:
+                return True
+            
+            visited.add(indx)
+            
+            op1 = indx + arr[indx]
+            op2 = indx - arr[indx]
+
+            if dp(op1) or dp(op2):
+                return True
+            
+            return False
+        
+        return dp(start)
