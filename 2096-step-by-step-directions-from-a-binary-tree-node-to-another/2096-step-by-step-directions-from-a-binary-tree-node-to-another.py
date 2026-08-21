f62005764c7d4f7e121f5317fdd4f89c4b983961
Anuj Singh <anujmaurya7967@gmail.com>
2026-08-21 16:23:25 +0530

    Time: 619 ms (6.44%), Space: 84.9 MB (8.62%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,57 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        
+
+        # first traveersal for parent mapping
+        parent_map = {root : None}
+        # curr_node , curr_lvl
+        queue = deque([(root , 0)])
+        parent_map = {root:None}
+        val_to_node = {root.val : root}
+
+        lvl_to_nodes = defaultdict(list)
+        deepest_lvl = 0
+        # get the deepest lvl nodes
+        while queue :
+            curr_node , curr_lvl = queue.popleft()
+            deepest_lvl = max(deepest_lvl , curr_lvl)
+            val_to_node[curr_node.val] = curr_node
+
+            lvl_to_nodes[curr_lvl].append(curr_node)
+
+            if curr_node.left :
+                queue.append((curr_node.left , curr_lvl+1))
+                parent_map[curr_node.left] = curr_node
+            if curr_node.right :
+                queue.append((curr_node.right , curr_lvl + 1))
+                parent_map[curr_node.right] = curr_node 
+        
+        startnode = val_to_node[startValue]
+        queue = deque([(startnode , "")])
+        vis = set()
+        vis.add(startnode)
+
+        while queue :
+            node , curr_path = queue.popleft()
+            if node == val_to_node[destValue] :
+                return curr_path
+            
+            if node.left and node.left not in vis :
+                vis.add(node.left)
+                queue.append((node.left , curr_path + "L"))
+            
+            if node.right and node.right not in vis :
+                vis.add(node.right)
+                queue.append((node.right , curr_path + "R"))
+            
+            if parent_map[node] and parent_map[node] not in vis :
+                vis.add(parent_map[node])
+                queue.append((parent_map[node] , curr_path + "U"))
+        
+    
