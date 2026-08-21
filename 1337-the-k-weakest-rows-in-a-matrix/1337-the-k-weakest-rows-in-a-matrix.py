e1f19a0cec443c43c494dbdbd3a9f333554a01f6
Anuj Singh <anujmaurya7967@gmail.com>
2026-08-21 14:46:18 +0530

    Time: 0 ms (100%), Space: 19.8 MB (12.29%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,17 +1,53 @@
+# import heapq
+
+# class Solution:
+#     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        
+#         n , m = len(mat) , len(mat[0])
+
+#         final = []
+#         heap = []
+#         ans = []
+#         for row in range(n) :
+#             count_1 = mat[row].count(1)
+#             heapq.heappush(heap , (count_1 , row))
+#             if len(heap) > n-k :
+#                 cnt , indx = heapq.heappop(heap)
+#                 ans.append(indx)
+#             # print(heap)
+#             # final.append((count_1 , row))
+        
+#         return ans
+#         # final.sort()
+
+#         # nlogn
+
+#         # ans = []
+#         # for i in range(k) :
+#         #     ans.append(heap[i][1])
+        
+#         # return ans
+import heapq
+from typing import List
+
+
 class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        
-        n , m = len(mat) , len(mat[0])
 
-        final = []
-        for row in range(n) :
-            count_1 = mat[row].count(1)
-            final.append((count_1 , row))
-        
-        final.sort()
+  def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+    heap = []
 
-        ans = []
-        for i in range(k) :
-            ans.append(final[i][1])
-        
-        return ans
+    for row in range(len(mat)):
+      count_1 = mat[row].count(1)
+      # Store negative values to invert min-heap into a max-heap
+      heapq.heappush(heap, (-count_1, -row))
+
+      if len(heap) > k:
+        heapq.heappop(heap)
+
+    # Pop remaining elements and reverse to get weakest first
+    ans = []
+    while heap:
+      cnt, indx = heapq.heappop(heap)
+      ans.append(-indx)
+
+    return ans[::-1]
