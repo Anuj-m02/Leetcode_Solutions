52f697ed121ec2f237845af80fd0e034c0cae939
Anuj Singh <anujmaurya7967@gmail.com>
2026-08-22 19:03:59 +0530

    Time: 5988 ms (5.97%), Space: 768.6 MB (5.97%) - LeetHub

---
+++ 2209-minimum-white-tiles-after-covering-with-carpets/2209-minimum-white-tiles-after-covering-with-carpets.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minimumWhiteTiles(self, floor: str, numCarpets: int, carpetLen: int) -> int:
+        
+        n = len(floor)
+
+        if numCarpets * carpetLen >= n :
+            return 0
+        
+
+        @lru_cache(maxsize=None)
+        def dp(indx , carpet_used) :
+
+            if indx >= n :
+                if carpet_used <= numCarpets :
+                    return 0
+                else :
+                    return float("inf")
+            
+
+            curr_tile = floor[indx]
+            ans = float("inf")
+            # option 1 dont place carpet here
+            if curr_tile == "1" :
+                ans = min(ans , 1 + dp(indx+1 , carpet_used))
+            else :
+                ans = min(ans , dp(indx+1 , carpet_used))
+            
+
+            # place carpte here
+            if curr_tile == "1" :
+                ans = min(ans , dp(indx + carpetLen , carpet_used+1))
+            else :
+                ans = min(ans , dp(indx + carpetLen , carpet_used+1))
+            
+            return ans
+        
+        return dp(0 , 0)
