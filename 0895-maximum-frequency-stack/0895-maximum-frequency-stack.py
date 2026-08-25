7efdfd6f81a39b468c2126420bcd0fef757ac70d
Anuj Singh <anujmaurya7967@gmail.com>
2026-08-25 11:47:00 +0530

    Time: 5091 ms (5.04%), Space: 29.4 MB (6.62%) - LeetHub

---
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.py
@@ -0,0 +1,95 @@
+# class FreqStack:
+
+#     def __init__(self):
+#         self.stack = []
+#         self.map1 = defaultdict(list)
+#         # {5 : [0 , 2 , 5] , ,7 :[1 , 3] , 4 :4}
+#         #(val : [all_indxs])
+#         self.map2 = {}
+#         # {5 : (3, 5) , 7 :{2 , 3} , 4:{1 , 4}}
+#         # (val : (count , recnt_indx))
+#         self.indx = 
+
+#     def push(self, val: int) -> None:
+        
+#         self.stack.append((val , indx))
+#         self.map1[self.val].append(indx)
+#         self.map2[self.val]
+        
+
+#     def pop(self) -> int:
+    
+
+
+# # Your FreqStack object will be instantiated and called as such:
+# # obj = FreqStack()
+# # obj.push(val)
+# # param_2 = obj.pop()
+
+from collections import defaultdict
+
+class FreqStack:
+
+    def __init__(self):
+        self.stack = []  
+        # (val, index)
+
+        self.map1 = defaultdict(list)
+        # val -> [all occurrence indices]
+
+        self.map2 = {}
+        # val -> [frequency, recent_index]
+
+        self.indx = 0
+
+    def push(self, val: int) -> None:
+
+        # Add to stack
+        self.stack.append((val, self.indx))
+
+        # Store occurrence index
+        self.map1[val].append(self.indx)
+
+        # Update frequency and recent index
+        if val not in self.map2:
+            self.map2[val] = [1, self.indx]
+        else:
+            self.map2[val][0] += 1
+            self.map2[val][1] = self.indx
+
+        self.indx += 1
+
+    def pop(self) -> int:
+
+        # Find:
+        # 1. maximum frequency
+        # 2. among those, maximum recent index
+
+        best_val = -1
+        best_freq = -1
+        best_index = -1
+
+        for val, (freq, recent_index) in self.map2.items():
+
+            if freq > best_freq:
+                best_freq = freq
+                best_index = recent_index
+                best_val = val
+
+            elif freq == best_freq and recent_index > best_index:
+                best_index = recent_index
+                best_val = val
+
+        # Remove latest occurrence of best_val
+        self.map1[best_val].pop()
+
+        # Update frequency and recent index
+        self.map2[best_val][0] -= 1
+
+        if self.map2[best_val][0] == 0:
+            del self.map2[best_val]
+            del self.map1[best_val]
+        else:
+            self.map2[best_val][1] = self.map1[best_val][-1]
+
+        return best_val
