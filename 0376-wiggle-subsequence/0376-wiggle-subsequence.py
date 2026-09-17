0630b9672317d109bc262b17ceb00acfa353bb11
Anuj Singh <anujmaurya7967@gmail.com>
2026-09-17 14:23:42 +0530

    Time: 99 ms (6.99%), Space: 20.3 MB (6.29%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -1,28 +1,48 @@
+# class Solution:
+#     def wiggleMaxLength(self, nums: list[int]) -> int:
+        
+#         n = len(nums)
+
+#         @lru_cache(maxsize=None)
+#         def dp(indx , prev_num , prev_parity) :
+#             if indx >= n :
+#                 return 0
+            
+#             # option 1 we can skip this num
+#             op1 = dp(indx+1 , prev_num , prev_parity)
+
+#             op2 = 0
+#             # we can pick if its alterantte
+#             curr_num = nums[indx]
+#             if prev_num is None :
+#                 op2 = 1 + dp(indx+1 , curr_num , -1)
+#             elif curr_num > prev_num and (prev_parity == 0 or prev_parity == -1) :
+#                 op2 += 1 + dp(indx+1 , curr_num , 1)
+#             elif curr_num < prev_num and (prev_parity == 1 or prev_parity == -1) :
+#                 op2 += 1 + dp(indx+1 , curr_num , 0)           
+
+#             return max(op1 , op2)
+        
+#         return dp(0 , None ,-1)
+
+from functools import lru_cache
+
 class Solution:
     def wiggleMaxLength(self, nums: list[int]) -> int:
-        
         n = len(nums)
+        if n < 2:
+            return n
 
+        # direction: 1 means we need an UPward step next, 0 means DOWNward
         @lru_cache(maxsize=None)
-        def dp(indx , prev_num , prev_parity) :
-            if indx >= n :
-                return 0
-            
-            # option 1 we can skip this num
-            op1 = dp(indx+1 , prev_num , prev_parity)
+        def dp(indx, is_up):
+            max_len = 0
+            for i in range(indx + 1, n):
+                if is_up and nums[i] > nums[indx]:
+                    max_len = max(max_len, 1 + dp(i, False))
+                elif not is_up and nums[i] < nums[indx]:
+                    max_len = max(max_len, 1 + dp(i, True))
+            return max_len
 
-            op2 = 0
-            # we can pick if its alterantte
-            curr_num = nums[indx]
-            if prev_num is None :
-                op2 = 1 + dp(indx+1 , curr_num , -1)
-            elif curr_num > prev_num and (prev_parity == 0 or prev_parity == -1) :
-                op2 += 1 + dp(indx+1 , curr_num , 1)
-            elif curr_num < prev_num and (prev_parity == 1 or prev_parity == -1) :
-                op2 += 1 + dp(indx+1 , curr_num , 0)           
-
-            return max(op1 , op2)
-        
-        return dp(0 , None ,-1)
-
-
+        # Try starting the sequence from index 0 either going UP next or DOWN next
+        return 1 + max(dp(0, True), dp(0, False))
