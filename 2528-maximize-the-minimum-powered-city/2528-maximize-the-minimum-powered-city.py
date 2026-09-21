428494311d9743e3b9bc9bbec424d36d9f427bca
Anuj Singh <anujmaurya7967@gmail.com>
2026-09-21 13:01:19 +0530

    Time: 1087 ms (80.99%), Space: 30.9 MB (76.06%) - LeetHub

---
+++ 2528-maximize-the-minimum-powered-city/2528-maximize-the-minimum-powered-city.py
@@ -0,0 +1,64 @@
+class Solution:
+    def maxPower(self, stations: list[int], r: int, k: int) -> int:
+        
+
+        n = len(stations)
+
+        power = [0]*(n)
+        window_sum = sum(stations[:min(n,r+1)])
+        power[0] = window_sum
+
+        for i in range(1,n) :
+            if i+r < n :
+                window_sum += stations[i+r]
+            
+            if i-r-1 >= 0 :
+                window_sum -= stations[i-r-1]
+            
+            power[i] = window_sum
+        
+        low = min(power)
+        high = sum(stations) + k
+        ans = low
+
+        def check(target) :
+            # diffrence array mthd
+            additions = [0]*(n+1)
+            curr_added = 0
+            needed_k = 0
+
+            for i in range(n):
+                curr_added += additions[i]
+                curr_power = power[i] + curr_added
+
+                if curr_power < target :
+                    diff = target - curr_power
+                    needed_k += diff
+                    if needed_k > k :
+                        return False
+                    
+                    curr_added += diff
+
+                    if i + 2*r + 1 < n :
+                        additions[i + 2*r + 1] -= diff
+            
+            return True
+
+
+        while low <= high :
+            mid = (low+high)//2
+            if check(mid) :
+                ans = mid
+                low = mid+1
+            else :
+                high = mid-1
+        
+        return ans
+
+        # get prefix sum
+        # then no_of_stations[indx] = prefix[indx+r] + prefix[indx-r]
+
+        # once we get initiall no_of_Statoins
+        # now we can add k power stations anywhere 
+        # so lets do binary search on maximum minim power stations if check(mid)
+        # check(mid) if k poerw station distributed optimially then return Truen or fLSE 
