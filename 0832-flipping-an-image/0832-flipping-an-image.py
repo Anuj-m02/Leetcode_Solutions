a163573841e071ff2757d294273f2704b6291f27
Anuj Singh <anujmaurya7967@gmail.com>
2026-09-22 12:06:44 +0530

    Time: 0 ms (100%), Space: 19.3 MB (24.19%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.py
+++ 0832-flipping-an-image/0832-flipping-an-image.py
@@ -1,15 +1,33 @@
+# class Solution:
+#     def flipAndInvertImage(self, image: list[list[int]]) -> list[list[int]]:
+        
+#         n = len(image)
+#         new_mat = []
+
+#         for row in image :
+#             new_row = row[::-1]
+#             new_mat.append(new_row)
+        
+#         for i in range(n):
+#             for j in range(n) :
+#                 new_mat[i][j] = 1 - new_mat[i][j]
+        
+#         return new_mat
+
 class Solution:
     def flipAndInvertImage(self, image: list[list[int]]) -> list[list[int]]:
-        
-        n = len(image)
-        new_mat = []
+        for row in image:
+            left, right = 0, len(row) - 1
+            
+            while left <= right:
+                if left == right:
+                    # Middle element in an odd-length row just needs to be inverted
+                    row[left] = 1 - row[left]
+                else:
+                    # Swap and invert elements simultaneously
+                    row[left], row[right] = 1 - row[right], 1 - row[left]
+                
+                left += 1
+                right -= 1
 
-        for row in image :
-            new_row = row[::-1]
-            new_mat.append(new_row)
-        
-        for i in range(n):
-            for j in range(n) :
-                new_mat[i][j] = 1 - new_mat[i][j]
-        
-        return new_mat
+        return image
