7ecd0921057b0affaffa6a5d4ed40498a950b7b8
Anuj Singh <anujmaurya7967@gmail.com>
2026-09-24 11:49:40 +0530

    Time: 49 ms (22.04%), Space: 22.7 MB (94.48%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,34 +1,3 @@
-# class Solution:
-#     def removeDuplicates(self, s: str, k: int) -> str:
-        
-#         # store char and cnt in stack
-#         stack = []
-#         n = len(s)
-
-#         for i in range(n) :
-#             curr_char = s[i]
-#             chk = False
-
-#             while stack and stack[-1][0] == curr_char :
-#                 if stack[-1][1] == k-1 :
-#                     chk = True
-#                     stack.pop()
-#                     # print(stack)
-
-#                 else :
-#                     curr_char , curr_cnt = stack.pop()
-#                     chk = True
-#                     stack.append((curr_char , curr_cnt+1))
-#                     # print(stack)
-            
-#             if not chk :
-#                 stack.append((curr_char , 1))
-        
-#         ans = ""
-#         for char , cnt in stack :
-#             ans += char*cnt
-#         return ans
-
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         
@@ -36,24 +5,27 @@ class Solution:
         stack = []
         n = len(s)
 
-        for i in range(n):
+        for i in range(n) :
             curr_char = s[i]
-            chk = False  # moved inside loop so it resets for s[i]
-            
-            if stack and stack[-1][0] == curr_char:
-                if stack[-1][1] == k - 1:
+            chk = False
+
+            if stack and stack[-1][0] == curr_char :
+                if stack[-1][1] == k-1 :
                     chk = True
                     stack.pop()
-                else:
-                    top_char, top_cnt = stack.pop()  # renamed to avoid overwriting curr_char
+                    # print(stack)
+
+                else :
+                    curr_char , curr_cnt = stack.pop()
                     chk = True
-                    stack.append((top_char, top_cnt + 1))
+                    stack.append((curr_char , curr_cnt+1))
+                    # print(stack)
             
-            if not chk:
-                stack.append((curr_char, 1))
+            if not chk :
+                stack.append((curr_char , 1))
         
         ans = ""
-        for char, cnt in stack:
-            ans += char * cnt  # multiply char by count
-            
+        for char , cnt in stack :
+            ans += char*cnt
         return ans
+
