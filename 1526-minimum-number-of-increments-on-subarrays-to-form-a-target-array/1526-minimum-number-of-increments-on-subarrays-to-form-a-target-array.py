d79a8392da5c45183bed3cd67ee8639a9122bfe1
Anuj Singh <anujmaurya7967@gmail.com>
2026-09-24 13:53:05 +0530

    Time: 79 ms (7.32%), Space: 29.9 MB (58.35%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -2,13 +2,31 @@ class Solution:
     def minNumberOperations(self, target: list[int]) -> int:
         
         n = len(target)
-        res , prev = 0, 0
-        for x in target :
-            if x > prev :
-                res += x-prev
+        # res , prev = 0, 0
+        # for x in target :
+        #     if x > prev :
+        #         res += x-prev
             
-            prev = x
+        #     prev = x
         
-        return res
+        # return res
 
-        #
+        stack = []
+        op = 0
+
+        for num in target :
+
+            # if curr_ele smaller
+            if stack and stack[-1] > num :
+                op += stack[-1]-num
+                while stack and stack[-1] > num :
+                    stack.pop()
+            
+            # print(stack)
+            
+            stack.append(num)
+        
+        if stack :
+            op += stack[-1]
+        
+        return op
